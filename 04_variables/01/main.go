024aa48f9e72d35f4d3db0512b0bf1eefe94f703
Olena <hellena.novikova2014@gmail.com>
2017-03-03 10:53:15 +0000

    added next folders

---
+++ 04_variables/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "Bob"
+	c := 3.111111111111
+	d := true
+	e := "1001"
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+}
---
+++ 04_variables/02/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "Bob"
+	c := 3.111111111111
+	d := true
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%v \n", e)
+}
+
+var e bool
