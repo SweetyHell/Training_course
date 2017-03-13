f956318ba8f6bc3238c25eb997aa9eabf26f1123
SweetyHell <hellena.novikova2014@gmail.com>
2017-03-13 10:22:15 +0000

    Structures

---
+++ Training_course/23_Data_structures/01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	First string
+	Last  string
+	Age   int
+}
+type Doublezero struct {
+	Person
+	First         string
+	Licencetokill bool
+}
+
+func main() {
+	p1 := Doublezero{
+		Person: Person{
+			First: "James",
+			Last:  "Bond",
+			Age:   20,
+		},
+		First:         "double zero seven",
+		Licencetokill: true,
+	}
+
+	fmt.Println(p1)
+}
